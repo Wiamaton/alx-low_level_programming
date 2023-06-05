6156d844053d3c3ae58963aca6a3546876f4ab27
Wiamaton <o.boughrarah@gmail.com>
2023-06-05 13:31:20 -0700

    C - More singly linked lists (Fixing tasks)

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,36 +1,72 @@
 #include "lists.h"
 
 /**
- * free_listint_safe - frees a linked list
- * @h: pointer to the first node in the linked list
+ * free_listp2 - frees a linked list
+ * @head: head of a list.
  *
- * Return: number of elements in the freed list
+ * Return: no return.
+ */
+void free_listp2(listp_t **head)
+{
+	listp_t *temp;
+	listp_t *curr;
+
+	if (head != NULL)
+	{
+		curr = *head;
+		while ((temp = curr) != NULL)
+		{
+			curr = curr->next;
+			free(temp);
+		}
+		*head = NULL;
+	}
+}
+
+/**
+ * free_listint_safe - frees a linked list.
+ * @h: head of a list.
+ *
+ * Return: size of the list that was freed.
  */
 size_t free_listint_safe(listint_t **h)
 {
-	size_t len = 0;
-	listint_t *current, *next;
+	size_t nnodes = 0;
+	listp_t *hptr, *new, *add;
+	listint_t *curr;
 
-	if (h == NULL || *h == NULL)
-		return (0);
-
+	hptr = NULL;
 	while (*h != NULL)
 	{
-		current = *h;
-		next = (*h)->next;
-		*h = NULL;
+		new = malloc(sizeof(listp_t));
 
-		if (current <= next)
+		if (new == NULL)
+			exit(98);
+
+		new->p = (void *)*h;
+		new->next = hptr;
+		hptr = new;
+
+		add = hptr;
+
+		while (add->next != NULL)
 		{
-			free(current);
-			len++;
-			break;
+			add = add->next;
+			if (*h == add->p)
+			{
+				*h = NULL;
+				free_listp2(&hptr);
+				return (nnodes);
+			}
 		}
 
-		free(current);
-		*h = next;
-		len++;
+		curr = *h;
+		*h = (*h)->next;
+		free(curr);
+		nnodes++;
 	}
 
-	return (len);
+	*h = NULL;
+	free_listp2(&hptr);
+	return (nnodes);
 }
