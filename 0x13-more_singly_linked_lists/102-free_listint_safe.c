e564d4170c5e404b475e9e31d5308b5250863d26
Wiamaton <o.boughrarah@gmail.com>
2023-06-06 07:58:46 -0700

    Fixing tasks

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -8,31 +8,31 @@
  */
 size_t free_listint_safe(listint_t **h)
 {
-        size_t len = 0;
-        listint_t *current, *next;
+	size_t len = 0;
+	listint_t *current, *next;
 
-        if (h == NULL || *h == NULL)
-                return (0);
+	if (h == NULL || *h == NULL)
+		return (0);
 
-        while (*h != NULL)
-        {
-                current = *h;
-                next = current->next;
+	while (*h != NULL)
+	{
+		current = *h;
+		next = current->next;
 
-                /* Check for a loop */
-                if (next >= current)
-                {
-                        *h = NULL;
-                        free(current);
-                        len++;
-                        break;
-                }
+		/* Check for a loop */
+		if (next >= current)
+		{
+			*h = NULL;
+			free(current);
+			len++;
+			break;
+		}
 
-                /* Free current node */
-                *h = next;
-                free(current);
-                len++;
-        }
+		/* Free current node */
+		*h = next;
+		free(current);
+		len++;
+	}
 
-        return (len);
+	return (len);
 }
