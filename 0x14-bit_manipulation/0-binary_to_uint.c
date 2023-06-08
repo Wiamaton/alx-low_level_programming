a79cb3e6d5c40411752ac2c11d082bc90c2fe3e8
Wiamaton <o.boughrarah@gmail.com>
2023-06-08 13:42:26 -0700

    Fixing errors

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -12,15 +12,15 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int ui = 0;
 
 	if (!b)
-		return 0;
+		return (0);
 
 	for (int i = 0; b[i] != '\0'; i++)
 	{
 		if (b[i] != '0' && b[i] != '1')
-			return 0;
+			return (0);
 
 		ui = (ui << 1) + (b[i] - '0');
 	}
 
-	return ui;
+	return (ui);
 }
