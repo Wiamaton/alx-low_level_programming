637baa85baf62d3f217e7b56c12a9f46c827617b
Wiamaton <o.boughrarah@gmail.com>
2023-07-10 09:39:40 -0700

    Fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,34 +1,39 @@
 #include "main.h"
+#include <fcntl.h>
+#include <unistd.h>
 
 /**
- * rewrite_file - Creates or overwrites a file with the given filename.
- * @filename: Pointer to the filename.
- * @text_content: String to be written to the file.
+ * create_file - Creates a file.
+ * @filename: A pointer to the name of the file to create.
+ * @text_content: A pointer to a string to write to the file.
  *
- * Return: 1 on success, -1 on failure.
+ * Return: If the function fails - -1.
+ *         Otherwise - 1.
  */
-int rewrite_file(const char *filename, char *text_content)
+int create_file(const char *filename, char *text_content)
 {
-	int file, i, wr;
+	int fd, nletters, rwr;
 
 	if (filename == NULL)
 		return (-1);
 
-	file = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
-	if (file == -1)
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
 		return (-1);
 
-	if (text_content)
-	{
-		i = 0;
-		while (text_content[i])
-			i++;
+	if (text_content == NULL)
+		text_content = "";
 
-		wr = write(file, text_content, i);
-		if (wr != i)
-			return (-1);
+	for (nletters = 0; text_content[nletters] != '\0'; nletters++)
+		;
+
+	rwr = write(fd, text_content, nletters);
+	if (rwr == -1)
+	{
+		close(fd);
+		return (-1);
 	}
 
-	close(file);
+	close(fd);
 	return (1);
 }
